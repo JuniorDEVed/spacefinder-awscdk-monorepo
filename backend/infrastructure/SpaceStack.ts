129923532a3342049da92478b36a6c52781c4fea
Sean Knowles <s@seanknowles.dev>
2021-07-11 02:16:29 +0100

    lambda factory and generic table finished

--- backend/infrastructure/SpaceStack.ts
+++ backend/infrastructure/SpaceStack.ts
@@ -1,16 +1,20 @@
-import { GenericTable } from "./GenericTable"
 import { Stack, StackProps } from "aws-cdk-lib"
-import { Construct } from "constructs"
-import { Code, Function as LambdaFunction, Runtime } from "aws-cdk-lib/lib/aws-lambda"
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/lib/aws-apigateway"
-import { join } from "path"
 import { PolicyStatement } from "aws-cdk-lib/lib/aws-iam"
-
 import { NodejsFunction } from "aws-cdk-lib/lib/aws-lambda-nodejs"
+import { Construct } from "constructs"
+import { join } from "path"
+
+import { GenericTable } from "./GenericTable"
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, "SpaceApi")
-  private spacesTable = new GenericTable("SpacesTable", "spaceId", this)
+
+  private spacesTable = new GenericTable(this, {
+    tableName: "SpacesTable",
+    primaryKey: "spaceId",
+    createLambdaPath: "Create",
+  })
 
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props)
@@ -19,6 +23,7 @@ export class SpaceStack extends Stack {
       entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
       handler: "handler",
     })
+
     const s3ListPolicy = new PolicyStatement()
     s3ListPolicy.addActions("s3:ListAllMyBuckets")
     s3ListPolicy.addResources("*")
@@ -28,5 +33,9 @@ export class SpaceStack extends Stack {
     const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs)
     const helloLamdbaResource = this.api.root.addResource("hello")
     helloLamdbaResource.addMethod("GET", helloLambdaIntegration)
+
+    // Spaces API integrations:
+    const spaceResource = this.api.root.addResource("spaces")
+    spaceResource.addMethod("POST", this.spacesTable.createLambdaIntagration)
   }
 }
--- backend/test/playground/hello.test.ts
+++ backend/test/playground/hello.test.ts
@@ -1,4 +1,4 @@
-import { handler } from "../../services/space-table/Create"
+import { handler } from "../../services/SpacesTable/Create"
 
 const event = {
   body: {
